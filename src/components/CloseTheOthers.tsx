554a4dd1f3a4e3dcb6ec16e79233f6826b0e0ad6
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-10-02 19:06:53 +0900

    style: 🎃 見た目を修正。flex box 完全に理解した

--- src/components/CloseTheOthers.tsx
+++ src/components/CloseTheOthers.tsx
@@ -1,12 +1,12 @@
 import { emit } from '@tauri-apps/api/event';
 
-const CloseTheOthers: React.FC = () => {
+const CloseTheOthers: React.FC<{ disabled: boolean }> = ({ disabled }) => {
   async function closeTheOthers() {
     await emit('quit', null);
   }
   return (
     <div>
-      <button type="button" onClick={closeTheOthers}>
+      <button type="button" onClick={closeTheOthers} disabled={disabled}>
         他の全てのモニターを閉じる
       </button>
     </div>
